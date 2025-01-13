99e4751e648eb80f9dea3ef008ffdcd9bfdd399b
Yezhyck <sergeysergeevytch@gmail.com>
2025-01-13 18:16:02 +0200

    Initial commit

---
+++ src/test/java/ua/yatsergray/university/management/system/UniversityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.yatsergray.university.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
